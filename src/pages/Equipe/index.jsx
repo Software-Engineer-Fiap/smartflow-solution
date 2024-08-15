8837574e866819f9d2275997cb7301e9649d17a5
giantcard5 <giantcard5.dev@outlook.com>
2024-08-15 17:09:34 -0300

    feat: Created Equipe Page

--- src/pages/Equipe/index.jsx
+++ src/pages/Equipe/index.jsx
@@ -2,9 +2,53 @@ import React from 'react';
 
 import './styles.scss';
 
+import EquipeData from '../../assets/equipe.json';
+
+import { 
+    Type 
+} from './data/types';
+import { 
+    subtractDates 
+} from './data/dates';
+
 const Equipe = () => {
     return (
-        <div></div>
+        <div className="container-equipe">
+            <div className="header-equipe">
+                <h2>Gestão da equipe</h2>
+                <span>Dashboard para monitoramento e gestão da equipe.</span>
+            </div>
+
+            <div className="content-equipe">
+                <tr>
+                    <th>Nome</th>
+                    <th>Código AT</th>
+                    <th>Descrição</th>
+                    <th>Senioridade</th>
+                    <th>Observação</th>
+                    <th>Editar</th>
+                </tr>
+
+                {EquipeData.map((item, key) => (
+                    <tr key={key}>
+                        <td>{item.NAME}</td>
+                        <td>{item.ID}</td>
+                        <td>
+                            <div className={`content-equipe-color-${Type[item.DESCRICAO]}`}>
+                                <span>{item.DESCRICAO}</span>
+                            </div>
+                        </td>
+                        <td>
+                            <div className={`content-equipe-color-${Type[item.SENIORIDADE]}`}>
+                                <span>{Type[item.SENIORIDADE]}</span>
+                            </div>
+                        </td>
+                        <td>{subtractDates(item.AUSENCIA_INI, item.AUSENCIA_FIN)} {item.AUSENCIA_TIPO ? `dias restantes de ${item.AUSENCIA_TIPO.toLowerCase()}` : 'Nenhuma'}</td>
+                        <td><button>Editar</button></td>
+                    </tr>
+                ))}
+            </div>
+        </div>
     );
 };
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,14 +5,20 @@ import {
     Routes
 } from 'react-router-dom';
 
+import Home from '../pages/Home';
+import Equipe from '../pages/Equipe';
+import Contratos from '../pages/Contratos';
+import Custos from '../pages/Custos';
+import Demandas from '../pages/Demandas';
+
 const Router = () => {    
     return (
         <Routes>
-            <Route path='/' element={<span>Home</span>}/>
-            <Route path='/equipe' element={<span>Equipe</span>}/>
-            <Route path='/contratos' element={<span>Contratos</span>}/>
-            <Route path='/custos' element={<span>Custos</span>}/>
-            <Route path='/demandas' element={<span>Demandas</span>}/>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/equipe' element={<Equipe/>}/>
+            <Route path='/contratos' element={<Contratos/>}/>
+            <Route path='/custos' element={<Custos/>}/>
+            <Route path='/demandas' element={<Demandas/>}/>
         </Routes>
     );
 };
