c5cb5672c4d48ca77e46f4d8df44919172951a53
getDanielDiniz <daniel.diniz.work@gmail.com>
2024-08-21 19:59:19 -0300

    Iniciando a pagina Demandas

--- src/pages/Demandas/index.jsx
+++ src/pages/Demandas/index.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
-
+import DemandasData from '../../assets/demandas.json';
 import './styles.scss';
+import { IoIosRefresh } from 'react-icons/io';
 
 const Demandas = () => {
+
     return (
-        <div></div>
+        <div className='container-demandas'>
+            <div className='header-demandas'>
+                <h2>Contrato vs Análise de Entrega</h2>
+                <div>
+                    <span>Esta página permite ao usuário comparar as discrepâncias entre contratos de venda e entrega de projetos atuais</span>
+                    <button className='button-demandas'>
+                        <IoIosRefresh />
+                        <span>
+                            Atualizar Dados
+                        </span>
+                    </button>
+                </div>
+            </div>
+
+            <table className='content-demandas'>
+                <thead>
+                    <tr>
+                        <th>ID do chamado</th>
+                        <th>Projeto</th>
+                        <th>Consultor</th>
+                        <th>Setor do Chamado</th>
+                        <th>Tipo de Chamado</th>
+                        <th>Complexidade</th>
+                        <th>Data de Abertura</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        DemandasData.map((item, key) => (
+                            item.STATUS_CHAMADO != "Closed" &&
+                            <tr key={key}>
+                                <td>
+                                    {item.CHAMADO}
+                                </td>
+                                <td>
+                                    {item.PROJETO}
+                                </td>
+                                <td>
+                                    {item.CONSULTOR}
+                                </td>
+                                <td>
+                                    {item.MODULO_CHAMADO}
+                                </td>
+                                <td>
+                                    {item.TIPO_CHAMADO}
+                                </td>
+                                <td>
+                                    {item.COMPLEXIDADE}
+                                </td>
+                                <td>
+                                    {item.DT_ABERTURA_CHAMADO}
+                                </td>
+                                <td>
+                                    {item.STATUS_CHAMADO}
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 };
 
